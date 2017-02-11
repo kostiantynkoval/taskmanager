bd3cf45dfb740d1641919d12626b7da56c163715
Kostiantyn Koval <kostiantyn.koval@gmail.com>
2017-02-11 22:49:05 +0200

    hope final

--- functions/deletetask.php
+++ functions/deletetask.php
@@ -4,6 +4,12 @@ $id  		= (isset($_GET['id']))?$_GET['id']:"";
 
 require_once 'model.php';
 
+$res 	= $pdo->query("SELECT `image` FROM `taskmanager` WHERE `id`='".$id."'");
+$task = $res->fetch();
+
 $res  = $pdo->query("DELETE FROM `taskmanager` WHERE id='".$id."'");
+if (file_exists('../'.$task['image'])) {
+		unlink('../'.$task['image']);
+};
 
 header('Location: ../index.php');
